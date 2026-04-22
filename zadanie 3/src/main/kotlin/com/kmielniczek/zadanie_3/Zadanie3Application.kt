d90d857ea38c7fe6d9e7cb0712b9d45aeec6318f
Kacper Mielniczek <mielniczekkasper@gmail.com>
2026-04-22 23:50:42 +0200

    initialize spring project

---
+++ zadanie 3/src/main/kotlin/com/kmielniczek/zadanie_3/Zadanie3Application.kt
@@ -0,0 +1,11 @@
+package com.kmielniczek.zadanie_3
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Zadanie3Application
+
+fun main(args: Array<String>) {
+	runApplication<Zadanie3Application>(*args)
+}
---
+++ zadanie 3/src/test/kotlin/com/kmielniczek/zadanie_3/Zadanie3ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kmielniczek.zadanie_3
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Zadanie3ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
