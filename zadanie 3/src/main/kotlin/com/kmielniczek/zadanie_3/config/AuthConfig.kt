36b04356d7cdc9df5a800ee87afe820d9aa843c8
Kacper Mielniczek <mielniczekkasper@gmail.com>
2026-04-23 23:33:23 +0200

    exercise 3 point 5 done

--- zadanie 3/src/main/kotlin/com/kmielniczek/zadanie_3/config/AuthConfig.kt
+++ zadanie 3/src/main/kotlin/com/kmielniczek/zadanie_3/config/AuthConfig.kt
@@ -4,11 +4,20 @@ import com.kmielniczek.zadanie_3.service.AuthService
 import com.kmielniczek.zadanie_3.service.MockAuthService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Lazy
+import org.springframework.context.annotation.Primary
 
 @Configuration
 class AuthConfig {
     @Bean
+    @Primary
     fun mockAuthService(): AuthService {
+        return MockAuthService()
+    }
+
+    @Bean
+    @Lazy
+    fun lazyMockAuthService(): AuthService {
         return MockAuthService()
     }
 }
